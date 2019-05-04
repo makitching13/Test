85b54d0858d6a41a768b091af6970c2cf6e74aa5
Lisa Zappia <lisa@Lisas-iMac.local>
2019-05-04 16:21:48 +1000

    Changed HTML to site

--- completeclearance/completeclearance/urls.py
+++ completeclearance/completeclearance/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('portfolio/', include('portfolio.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
